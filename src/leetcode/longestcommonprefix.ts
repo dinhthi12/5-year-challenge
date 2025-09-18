19fec7f7d737551fbff49a28c00fd13f6f2cb64d
dinhthi12 <thi884641@gmail.com>
2025-09-18 23:15:27 +0700

    Longest common prefix

--- src/leetcode/longestcommonprefix.ts
+++ src/leetcode/longestcommonprefix.ts
@@ -1,7 +1,7 @@
 export function longestCommonPrefix(strs: string[]): string {
+  if(strs.length === 0) return "";
   // Assume the initial prefix is string in the array
-  // Use "!" to tell TS to make sure strs[0] exists (if the array is empty, check first)
-  let prefix = strs[0]!;
+  let prefix: string = strs[0]!;
 
   // Iterate over each remaining string in the array
   for (let i = 1; i < strs.length; i++) {
@@ -10,7 +10,7 @@ export function longestCommonPrefix(strs: string[]): string {
       // Trim prefix by 1 character from the end
       prefix = prefix.substring(0, prefix.length - 1);
 
-      // If prefix is ​​empty, there is no common prefix → return
+      // If prefix is common prefix → return
       if (prefix === "") return "";
     }
   }
